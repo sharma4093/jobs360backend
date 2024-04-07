23493b1c5c99d957b1142988c7a306617ca12084
Prashant <pra4093@gmail.com>
2024-04-07 23:51:47 +0530

    second

--- server.js
+++ server.js
@@ -1,12 +1,23 @@
+// require ('dotenv').config();
 const express = require('express');
 const app = express();
 
+// console.log(process.env);
+const port =  5050;
 
 
-const port = 5050;
+app.get('/', (req,res)=>{
 
-api.get('/detils', (req,res)=>{
-    res.send("conncected ")
+    res.send("welcome home");
+})
+
+app.get('/getdetail', (req,res)=>{
+    const details = [{
+        "id":1,
+        "details":"this",
+        "xzy": "hell"
+    }]
+    res.send(details)
 })
 
 app.listen(port, ()=>{
